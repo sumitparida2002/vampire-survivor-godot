0ee6caefc54e4567bea0b48a2abe120c5a896e99
Sumit Parida <80693203+sumitparida2002@users.noreply.github.com>
2024-03-25 11:07:15 -0400

    first commit

---
+++ mob.gd
@@ -0,0 +1,26 @@
+extends CharacterBody2D
+
+
+@onready var player=get_node("/root/Game/Player");
+
+var health=3
+
+func _ready():
+	%Slime.play_walk()
+
+func _physics_process(delta):
+	var direction=global_position.direction_to(player.global_position)
+	velocity=direction*300.0
+	move_and_slide()
+	
+	
+func take_damage():
+	health-=1
+	%Slime.play_hurt()
+	
+	if health==0:
+		queue_free()
+	const SMOKE_SCREEN=preload("res://smoke_explosion/smoke_explosion.tscn")
+	var smoke=SMOKE_SCREEN.instantiate()
+	get_parent().add_child(smoke)
+	smoke.global_position=global_position
